0314fad2ce554df05a0781f74f0051357cdbdc43
MASUD-01 <masudrana96141@gmail.com>
2022-09-02 23:41:51 +0600

    üpdate add

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 let random = require('./randomUser')
 const cors = require('cors')
 
