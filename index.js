73645a2251003f9a60de7fa9349e1e7822445494
MASUD-01 <masudrana96141@gmail.com>
2022-09-03 12:44:28 +0600

    update code inside routes folder

--- index.js
+++ index.js
@@ -3,51 +3,17 @@ const app = express()
 const port = process.env.PORT || 5000
 let random = require('./randomUser')
 const cors = require('cors')
+const router = require('./routes/user.route')
 
 
 //middelware
 app.use(cors());
 app.use(express.json());
 
+app.use('/user', router)
 
 
-app.get('/user/all', (req, res) => {
-    const { limit } = req.query;
-    console.log(limit)
-    res.send(random.slice(0, limit))
-})
-app.get('/user/random', (req, res) => {
 
-    function randomUserName() {
-        const randomUser = Math.floor(Math.random() * random.length)
-        const userName = random[randomUser]
-        return userName
-    }
-    const userName = randomUserName()
-    res.send(userName)
-})
-
-app.post('/user/save', (req, res) => {
-    const user = req.body
-    random.push(user)
-    res.send(user)
-})
-
-
-app.patch("/user/:id", (req, res) => {
-    const { id } = req.params;
-    const newDatas = random.find(data => data.id == id);
-    newDatas.id = id;
-    newDatas.name = req.body.name;
-    newDatas.contact = req.body.contact;
-    res.send(newDatas)
-})
-
-app.delete('/user/:id', (req, res) => {
-    const { id } = req.params;
-    random = random.filter(user => user.id !== Number(id))
-    res.send(random)
-})
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
---
+++ routes/user.route.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router()
+let random = require('../randomUser')
+
+router.get('/all', (req, res) => {
+    const { limit } = req.query;
+    console.log(limit)
+    res.send(random.slice(0, limit))
+})
+router.get('/random', (req, res) => {
+    function randomUserName() {
+        const randomUser = Math.floor(Math.random() * random.length)
+        const userName = random[randomUser]
+        return userName
+    }
+    const userName = randomUserName()
+    res.send(userName)
+})
+
+router.post('/save', (req, res) => {
+    const user = req.body
+    random.push(user)
+    res.send(random)
+})
+
+router.put('/user/bulk-update', (req, res) => {
+    const { id } = req.params;
+    const newDatas = random.find(data => data.id == id);
+})
+
+router.route("/:id").patch((req, res) => {
+    const { id } = req.params;
+    const newDatas = random.find(data => data.id == id);
+    newDatas.id = id;
+    newDatas.name = req.body.name;
+    newDatas.gender = req.body.gender;
+    newDatas.address = req.body.address;
+    newDatas.photoUrl = req.body.photoUrl;
+    newDatas.contact = req.body.contact;
+    res.send(newDatas)
+}).delete((req, res) => {
+    const { id } = req.params;
+    random = random.filter(user => user.id !== Number(id))
+    res.send(random)
+})
+module.exports = router;
